3eb501f4d8c1fd1554040df1ac3db99960d0c121
rg <ryangehrin@gmail.com>
2014-07-01 00:12:47 -0700

    minimize total wait time

---
+++ c18/p2.java
@@ -0,0 +1,13 @@
+// minimize total wait time
+import java.util.Arrays;
+
+
+class P2 {
+
+  public static void main(String[] args) {
+    int[] schedule = {1,4,3,7,9,2} ;
+    Arrays.sort(schedule) ;
+    System.out.println(Arrays.toString(schedule) );
+  }
+
+}
